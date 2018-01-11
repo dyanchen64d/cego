f57ea4f7061ec4651e6b16afa792077e68abd441
zc <zhangchen40@126.com>
2018-01-11 17:13:38 +0800

    config remove

--- app.js
+++ app.js
@@ -1,11 +1,33 @@
+const config = require('./config.js')
+
 //app.js
 App({
   onLaunch: function () {
-    wx.getUserInfo({
-      success (res) {
-        console.log(res);
+    // wx.getUserInfo({
+    //   success (res) {
+    //     console.log(res);
+    //   }
+    // })
+    wx.login({
+      success: function(res) {
+        if (res.code) {
+          console.log(res.code);
+          //发起网络请求
+          wx.request({
+            url: config.domain + '/getSessionKeyAndOpenId',
+            method: 'POST',
+            data: {
+              code: res.code
+            },
+            header: {
+              'content-type': 'application/json' // 默认值
+            }
+          })
+        } else {
+          console.log('获取用户登录态失败！' + res.errMsg)
+        }
       }
-    })
+    });
   },
   globalData: {
     userInfo: null
